64bc489db879898e385fd28b2740bf03c0605fd3
cahyaind <cahyaindsi@gmail.com>
2022-08-11 21:25:23 +0700

    endpoint api function update and destroy success

--- app/Http/Controllers/TourApiController.php
+++ app/Http/Controllers/TourApiController.php
@@ -21,4 +21,16 @@ class TourApiController extends Controller
         $tour = Tour::create($request->all());
         return response()->json(['message' => 'Powodzenie', 'data' => $tour]);
     }
+
+    public function update(Request $request,$id) {
+        $tour = Tour::find($id);
+        $tour->update($request->all());
+        return response()->json(['message' => 'Powodzenie', 'data' => $tour]);
+    }
+
+    public function destroy($id) {
+        $tour = Tour::find($id);
+        $tour->delete();
+        return response()->json(['message' => 'Successfully deleted','data' => null]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,7 @@ use App\Http\Controllers\TourApiController;
 */
 
 Route::get('/tours',[TourApiController::class,'index']);
-Route::post('/tours',[TourApiController::class,'store']);
-Route::get('/tours/{id}/edit',[TourApiController::class, 'edit']);
 Route::get('/tours/{id}',[TourApiController::class, 'show']);
+Route::post('/tours',[TourApiController::class,'store']);
 Route::put('/tours/{id}',[TourApiController::class, 'update']);
 Route::delete('/tours/{id}',[TourApiController::class, 'destroy']);
